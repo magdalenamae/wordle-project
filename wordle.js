e59baca89ee816a6d1988d50fabf22f3f71a29a6
magdalena <@gmail.com>
2022-03-23 17:59:17 +1100

    update

--- wordle.js
+++ wordle.js
@@ -1,4 +1,4 @@
-const targetWord = 'allow'
+const targetWord = 'world'
 
 console.log(targetWord)
 let columnIndex = 0
@@ -7,8 +7,8 @@ let columnIndex = 0
 let input = document.getElementById('input')
 console.log(input)
 
-let row = document.querySelectorAll('.row')
-console.log(row)
+let rows = document.querySelectorAll('.row')
+console.log(rows)
 // acess each row speacidically 
 // acces all as children 
 //update output row in each itteration of the for loop 
@@ -16,65 +16,83 @@ console.log(row)
 
 let rowIndex = 0
 
-        input.addEventListener('keypress', function(e) {
-            
-            let guessWord = input.value
-                columnIndex = 0
-
-                for (let index in guessWord){
-                    let outputRow = row[rowIndex].children
-                    if (guessWord.length <= 5){
-                        
-                        let inputCharacter = guessWord[index]
-                        let outputElement = outputRow[index]
-                        let targetCharacter = targetWord[index]
-                
-                        outputElement.textContent = inputCharacter 
-                    
-                        columnIndex += 1
-                        // when word is typed and button is clicked grab aeverything you need from input box
-                        //submit button seperate
-                        if (guessWord.length == 5 && guessWord == targetCharacter){
-
-                            submit.addEventListener('click', function() {
-                                outputElement.classList.add('correct')
-                            })                               
-                        
-                        } else if (inputCharacter == targetCharacter){
-                            submit.addEventListener('click', function() {
-                            outputElement.classList.add('correct')
-                        })
-
-                        } else if(guessWord != targetWord){
-                            // console.log(outputElement)
-                            outputElement.classList.remove('correct')
-                        } 
-                        
-                    
-                    }  
-                    
-                    
-                
-                }
-
-                    input.addEventListener('change',function(){
-                    input.value='';
-                     },false);
-
-        }); 
-        
-        submit.addEventListener('click', function(){
-            const submit = document.getElementById('submit')
-            
-            // outputElement.classList.add('correct')
-            // get word that is submit 
-            // check agaisnt target word and each position 
-            rowIndex += 1
-            console.log(rowIndex)
-            
-        })
+// on each key press display key value in box 
+// display each letter in each div(letter)
+input.addEventListener('input', function(e){
+    let textValue = input.value
+    // index is the positon number     
+    for(index in textValue){
+       
+        let outputRow = rows[rowIndex].children
+        let character = textValue[index]
+        let box = outputRow[index]
+        box.textContent = character
 
         
+
+    
+    }
+    
+    console.log(textValue)
+    // outputElement.textContent = inputCharacter
+    
+})
+  
+// check the inputs conditonns and apply the nesscary class
+// outputElement.classList.add('correct')
+// get word that is submit 
+// check agaisnt target word and each position   
+//display message 'you won' 
+// goes to new row 
+// updates color class   
+submit.addEventListener('click', function(){
+    const submit = document.getElementById('submit')
+    // guess word = letters in field             
+    let guessWord = input.value
+    columnIndex = 0
+    // postion of each letter in input  
+    for (let index in guessWord){
+        // the set of divs in each row
+        let outputRow = rows[rowIndex].children
+        if (guessWord.length <= 5){
+            
+            // each character in the guesword - user types in 
+            let inputCharacter = guessWord[index]
+            // each div in the row  
+            let outputElement = outputRow[index]
+            // each letter in the target word 
+            let targetCharacter = targetWord[index]
+        
+            // columnIndex += 1
+            // when word is typed and button is clicked grab aeverything you need from input box
+            //submit button seperate
+           if (inputCharacter == targetCharacter){
+              
+                outputElement.classList.add('correct')
+
+            } else if (targetWord.includes(inputCharacter)){
+                console.log('i work')
+                outputElement.classList.add('halfcorrect')
+
+            }else if(guessWord != targetWord){
+                // console.log(outputElement)
+                outputElement.classList.remove('correct')
+
+            }
+            
+            
+        }  
+        
+    input.value = '';
+    console.log(input.value)
+
+    }
+  
+    rowIndex += 1
+    console.log(rowIndex)
+   
+})
+
         
 
     
